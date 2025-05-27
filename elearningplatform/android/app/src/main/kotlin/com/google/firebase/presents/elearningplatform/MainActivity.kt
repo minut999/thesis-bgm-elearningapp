d76a335d536303b73f7a9646ed4ca7fd27a2e225
minut999 <lolomonyonyi0509@gmail.com>
2025-05-27 19:18:45 +0800

    first commit

---
+++ elearningplatform/android/app/src/main/kotlin/com/google/firebase/presents/elearningplatform/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.google.firebase.presents.elearningplatform
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Nigga Website',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Financial E-Learning Web Demo'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Welcome mga niggas!',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('Button clicked!')),
+                )
+              },
+              child: const Text('Click Me'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
